453b5e2fe660d4c2f91692cdf7b0466f48d2696b
lnguyend-agai <nguyendinhloc126@gmail.com>
2025-01-16 22:10:31 +0700

    [FEAT] Stage-2: Add arrow so that user can know that is a expander

--- src/pages/SportsPage.js
+++ src/pages/SportsPage.js
@@ -14,7 +14,10 @@ const SportsPage = () => {
       {/* Expander for Badminton */}
       <div className="expander">
         <input type="checkbox" id="badminton" />
-        <label htmlFor="badminton">Cầu lông</label>
+        <label htmlFor="badminton">
+          Cầu lông
+          <span className="arrow">&#9660;</span>
+        </label>
         <div className="content">
           <p>Cầu lông là một trong những môn thể thao phổ biến tại Việt Nam.</p>
           <img src={badmintonImage} alt="Cầu lông" />
@@ -24,7 +27,10 @@ const SportsPage = () => {
       {/* Expander for Swimming */}
       <div className="expander">
         <input type="checkbox" id="swimming" />
-        <label htmlFor="swimming">Bơi lội</label>
+        <label htmlFor="swimming">
+          Bơi lội
+          <span className="arrow">&#9660;</span>
+        </label>
         <div className="content">
           <p>Bơi lội là môn thể thao mang về nhiều huy chương cho Việt Nam.</p>
           <img src={swimmingImage} alt="Bơi lội" />
@@ -34,7 +40,10 @@ const SportsPage = () => {
       {/* Expander for Football */}
       <div className="expander">
         <input type="checkbox" id="football" />
-        <label htmlFor="football">Bóng đá</label>
+        <label htmlFor="football">
+          Bóng đá
+          <span className="arrow">&#9660;</span>
+        </label>
         <div className="content">
           <p>Bóng đá là môn thể thao vua tại Việt Nam, với nhiều đội tuyển nổi bật.</p>
           <img src={footballImage} alt="Bóng đá" />
