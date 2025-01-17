4322f575a5c01ac2730d15b85619ad8f5af18d99
lnguyend-agai <nguyendinhloc126@gmail.com>
2025-01-17 21:51:58 +0700

    [FEAT] Stage-2: Edit expander of sport and music page

--- src/pages/MusicPage.js
+++ src/pages/MusicPage.js
@@ -1,50 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MusicPage.css';
 
 const musicData = [
   {
     title: 'Anh Trai Say Hi',
     links: [
-      'https://youtube.com/watch?v=d9Fohvdet4g', 
-      'https://www.youtube.com/watch?v=a-jdOviGQ00',
+      'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      'https://www.youtube.com/watch?v=3JZ_D3ELwOQ',
     ],
   },
   {
     title: 'Anh Trai Vượt Ngàn Chông Gai',
     links: [
-      'https://www.youtube.com/watch?v=nXiwlKJSkHY', 
-      'https://www.youtube.com/watch?v=3UbQjlHbPsE',
+      'https://www.youtube.com/watch?v=2Vv-BfVoq4g',
+      'https://www.youtube.com/watch?v=fJ9rUzIMcZQ',
     ],
   },
   {
-    title: 'The Masked Singer',
+    title: 'Ca Sĩ Giấu Mặt',
     links: [
-      'https://www.youtube.com/watch?v=b8dC2QZKSNs', 
+      'https://www.youtube.com/watch?v=QH2-TGUlwu4',
     ],
   },
 ];
 
 const MusicPage = () => {
+  // Khởi tạo trạng thái mở mặc định cho tất cả expander
+  const [openExpanders, setOpenExpanders] = useState(
+    Array(musicData.length).fill(true)
+  );
+
+  const toggleExpander = (index) => {
+    setOpenExpanders((prevState) =>
+      prevState.map((isOpen, idx) => (idx === index ? !isOpen : isOpen))
+    );
+  };
+
   return (
     <div className="music-page">
       <h1>Trang Âm Nhạc</h1>
 
       {musicData.map((item, index) => (
-        <div className="expander" key={index}>
-          <input type="checkbox" id={`expander-${index}`} />
-          <label htmlFor={`expander-${index}`}>
+        <div
+          className={`expander ${openExpanders[index] ? 'open' : ''}`}
+          key={index}
+        >
+          <label onClick={() => toggleExpander(index)}>
             {item.title}
-            <span className="arrow">&#9660;</span>
+            <span className="arrow">
+              {openExpanders[index] ? '▲' : '▼'}
+            </span>
           </label>
-          <div className="content">
-            {item.links.map((link, idx) => (
-              <div key={idx} className="youtube-link">
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                  {`Link bài hát ${idx + 1}`}
-                </a>
-              </div>
-            ))}
-          </div>
+          {openExpanders[index] && (
+            <div className="content">
+              {item.links.map((link, idx) => (
+                <div key={idx} className="youtube-link">
+                  <a href={link} target="_blank" rel="noopener noreferrer">
+                    {`Link bài hát ${idx + 1}`}
+                  </a>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       ))}
     </div>
--- src/pages/SportsPage.js
+++ src/pages/SportsPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SportsPage.css';
 
 // Import ảnh của các vận động viên và đội tuyển
@@ -7,47 +7,65 @@ import swimmingImage from '../assets/swimming.jpg';
 import footballImage from '../assets/football.jpg';
 
 const SportsPage = () => {
+  // Trạng thái mặc định mở của từng Expander
+  const [expanders, setExpanders] = useState({
+    badminton: true,
+    swimming: true,
+    football: true,
+  });
+
+  // Toggle trạng thái mở/đóng của từng Expander
+  const toggleExpander = (key) => {
+    setExpanders((prevState) => ({
+      ...prevState,
+      [key]: !prevState[key],
+    }));
+  };
+
   return (
     <div className="sports-page">
       <h1>Thể thao Việt Nam</h1>
 
       {/* Expander for Badminton */}
       <div className="expander">
-        <input type="checkbox" id="badminton" />
-        <label htmlFor="badminton">
+        <label onClick={() => toggleExpander('badminton')}>
           Cầu lông
-          <span className="arrow">&#9660;</span>
+          <span className={`arrow ${expanders.badminton ? 'open' : ''}`}>&#9660;</span>
         </label>
-        <div className="content">
-          <p>Cầu lông là một trong những môn thể thao phổ biến tại Việt Nam.</p>
-          <img src={badmintonImage} alt="Cầu lông" />
-        </div>
+        {expanders.badminton && (
+          <div className="content">
+            <p>Cầu lông là một trong những môn thể thao phổ biến tại Việt Nam.</p>
+            <img src={badmintonImage} alt="Cầu lông" />
+          </div>
+        )}
       </div>
 
       {/* Expander for Swimming */}
       <div className="expander">
-        <input type="checkbox" id="swimming" />
-        <label htmlFor="swimming">
+        <label onClick={() => toggleExpander('swimming')}>
           Bơi lội
-          <span className="arrow">&#9660;</span>
+          <span className={`arrow ${expanders.swimming ? 'open' : ''}`}>&#9660;</span>
         </label>
-        <div className="content">
-          <p>Bơi lội là môn thể thao mang về nhiều huy chương cho Việt Nam.</p>
-          <img src={swimmingImage} alt="Bơi lội" />
-        </div>
+        {expanders.swimming && (
+          <div className="content">
+            <p>Bơi lội là môn thể thao mang về nhiều huy chương cho Việt Nam.</p>
+            <img src={swimmingImage} alt="Bơi lội" />
+          </div>
+        )}
       </div>
 
       {/* Expander for Football */}
       <div className="expander">
-        <input type="checkbox" id="football" />
-        <label htmlFor="football">
+        <label onClick={() => toggleExpander('football')}>
           Bóng đá
-          <span className="arrow">&#9660;</span>
+          <span className={`arrow ${expanders.football ? 'open' : ''}`}>&#9660;</span>
         </label>
-        <div className="content">
-          <p>Bóng đá là môn thể thao vua tại Việt Nam, với nhiều đội tuyển nổi bật.</p>
-          <img src={footballImage} alt="Bóng đá" />
-        </div>
+        {expanders.football && (
+          <div className="content">
+            <p>Bóng đá là môn thể thao vua tại Việt Nam, với nhiều đội tuyển nổi bật.</p>
+            <img src={footballImage} alt="Bóng đá" />
+          </div>
+        )}
       </div>
     </div>
   );
